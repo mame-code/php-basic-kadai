9d3af9485927b5253f8b104523243b06f709f523
mame-code <149633750+mame-code@users.noreply.github.com>
2024-01-01 21:09:35 +0900

    課題007提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<heda>
+  <meta charset="UTF-8">
+  <title>課題007</title>
+</head>
+
+<body>
+  <p>
+  <?php
+  $vegitabl_data = ['name' => 'onion','price' => 200,'weight' => 160];
+
+  print_r($vegitabl_data);
+  ?>
+</P>
+</body>
+</html>
